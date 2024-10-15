4328cbef16264fcae15c36d4b50f0388889937be
NordicMadViking <stian@monkeytech.com.au>
2024-10-15 11:10:01 +0800

    Auto Commit

--- IBM_DB_Adapter/ibm_db/lib/active_record/connection_adapters/ibm_db_adapter.rb
+++ IBM_DB_Adapter/ibm_db/lib/active_record/connection_adapters/ibm_db_adapter.rb
@@ -3411,13 +3411,7 @@ module ActiveRecord
         end
 
         if @servertype.instance_of? IBM_IDS #mtech
-          unique_info = internal_exec_query(<<~SQL, "SCHEMA")
-          SELECT 
-          SELECT KEYCOL.CONSTNAME, KEYCOL.COLNAME FROM SYSCAT.KEYCOLUSE KEYCOL
-              INNER JOIN SYSCAT.TABCONST TABCONST ON KEYCOL.CONSTNAME=TABCONST.CONSTNAME
-              WHERE TABCONST.TABSCHEMA=#{quote(schema_name.upcase)} and
-              TABCONST.TABNAME=#{quote(table_name.upcase)} and TABCONST.TYPE='U'
-        SQL 
+
           
           else
         unique_info = internal_exec_query(<<~SQL, "SCHEMA")
