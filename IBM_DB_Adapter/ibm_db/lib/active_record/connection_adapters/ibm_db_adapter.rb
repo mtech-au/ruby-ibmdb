be4ca52ac7ff20e5ed4b0f77c3e8771df4bd06c0
NordicMadViking <stian@monkeytech.com.au>
2024-11-26 16:10:01 +0800

    Auto Commit

--- IBM_DB_Adapter/ibm_db/lib/active_record/connection_adapters/ibm_db_adapter.rb
+++ IBM_DB_Adapter/ibm_db/lib/active_record/connection_adapters/ibm_db_adapter.rb
@@ -118,7 +118,7 @@ module ActiveRecord
       private
 
       def visit_TableDefinition(o)
-        return super unless @conn.class == IBM_DBAdapter # mtech - crashes PostgreSQLAdapter
+        # return super unless @conn.class == IBM_DBAdapter # mtech - crashes PostgreSQLAdapter
 
         create_sql = +"CREATE#{table_modifier_in_create(o)} TABLE "
         create_sql << 'IF NOT EXISTS ' if o.if_not_exists
